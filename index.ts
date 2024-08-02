db1fdc786dfe0998fececbdd064ba21b576372a3
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-02 07:10:30 -0300

    changed client to use only websocket

--- index.ts
+++ index.ts
@@ -2,41 +2,53 @@ import "dotenv/config";
 import { Socket, io } from "socket.io-client";
 import type { ClientToServerEvents, ServerToClientEvents } from "./types";
 
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
 async function main() {
-    const bearer = await fetch(`${process.env.WEBHOOK_TESTER_URL!}/users/login`, {
-        method: "POST",
-        body: JSON.stringify({
-            email: process.env.EMAIL!,
-            password: process.env.PASSWORD!,
-        }),
-        headers: {
-            "content-type": "application/json; charset=utf-8",
-        },
-    });
-    if (bearer.status != 201) {
-        throw new Error(`bearer fetch status: ${bearer.status}, ${await bearer.text()}`);
-    }
-    const accessToken = (await bearer.json()).accessToken;
-    if (typeof accessToken !== "string") {
-        throw new Error("could not get accessToken");
-    }
-
-    const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-        process.env.WEBHOOK_TESTER_URL!,
-        {
-            auth: {
-                token: accessToken,
+    try {
+        const bearer = await fetch(`${process.env.WEBHOOK_TESTER_URL!}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify({
+                email: process.env.EMAIL!,
+                password: process.env.PASSWORD!,
+            }),
+            headers: {
+                "content-type": "application/json; charset=utf-8",
             },
+        });
+        if (bearer.status != 201) {
+            throw new Error(`bearer fetch status: ${bearer.status}, ${await bearer.text()}`);
         }
-    );
-    // socket.auth
-    socket.on("connect", () => {
-        console.log(`you connected with ${socket.id}`);
-    });
+        const accessToken = (await bearer.json()).accessToken;
+        if (typeof accessToken !== "string") {
+            throw new Error("could not get accessToken");
+        }
 
-    socket.on("message", (data) => {
-        console.log(data);
-    });
+        const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+            process.env.WEBHOOK_TESTER_URL!,
+            {
+                auth: {
+                    token: accessToken,
+                },
+                transports: ["websocket"],
+            }
+        );
+        socket.on("connect_error", (error) => {
+            // ...
+            console.log("ocorreu um erro: ", error);
+        });
+        // socket.auth
+        console.log("aqui2");
+        socket.on("connect", () => {
+            console.log(`you connected with ${socket.id}`);
+        });
+
+        socket.on("message", (data) => {
+            console.log(data);
+        });
+    } catch (err) {
+        console.log("error: ", err);
+    }
 }
 
 main();
