7882f328678130873c1f245be90f1cfcf3558bf5
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-04 07:41:19 -0300

    finally it's working

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ import "dotenv/config";
 import { Socket, io } from "socket.io-client";
 import type { ClientToServerEvents, ServerToClientEvents } from "./types";
 
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-
 async function main() {
     try {
         const bearer = await fetch(`${process.env.WEBHOOK_TESTER_URL!}/auth/login`, {
@@ -38,13 +36,27 @@ async function main() {
             console.log("ocorreu um erro: ", error);
         });
         // socket.auth
-        console.log("aqui2");
         socket.on("connect", () => {
             console.log(`you connected with ${socket.id}`);
         });
 
+        const headersList = {
+            Accept: "*/*",
+            "Content-Type": "application/json",
+        };
+
         socket.on("message", (data) => {
-            console.log(data);
+            console.log(JSON.stringify(data.payload));
+            const fetchURL = process.env.POST_URL! + data.extra_url;
+            console.log("Posting on URL: " + fetchURL);
+
+            fetch(fetchURL, {
+                method: "POST",
+                body: JSON.stringify(data.payload),
+                headers: headersList,
+            })
+                .then((res) => console.log("O resultado do fetch: " + res.status))
+                .catch((err) => console.error(err));
         });
     } catch (err) {
         console.log("error: ", err);
